92d19b6bb230a5db08b9edaa378a1169b4a372c2
TZG <recordmp3@users.noreply.github.com>
2017-12-02 21:50:39 +0800

    绘图

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from bidder import Bidder
 from strategy import Strategy
 from mechanism import * 
 from valuation_generator import uniformValuationGenerator
+from draw import Draw
 def main():
 
     n = 10
@@ -14,8 +15,17 @@ def main():
     ctrs = [0.1 for i in range(10)]
     auction = Auction(bidders, GSP(ctrs))
     n_auction = 10000
+    x_axis,bidders_revenue,auction_revenue = [],[],[]
     for i in range(n_auction):
         auction.takeAuction()
+        x_axis.append(i+1)
+        sum = np.sum(np.array([bidders[i].revenue for i in range(n)]))/n
+        bidders_revenue.append(sum)            #  目前取平均值
+        auction_revenue.append(auction.revenue)
+        
+    Draw(x_axis,bidders_revenue,'times','revenue',"bidders_revenue")
+    Draw(x_axis,auction_revenue,'times','revenue',"auction_revenue")
+
     print([bidders[i].revenue for i in range(n)])
     print(auction.revenue)
 
